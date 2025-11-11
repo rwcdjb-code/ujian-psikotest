ef3c8caba18342f64a1e636a8632926594130264
rwcdjb-code <rwcdjb@gmail.com>
2025-11-11 15:24:27 +0700

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // GANTI jika perlu, tapi sudah diisi dengan URL Apps Script yang kamu kirim
-const BASE_URL = "https://script.google.com/macros/s/AKfycbxkdZiHskbhNNaTYhBz7xhZwFoTxzdwmYFgIIAhgnRHKs4aPDhsCHItV1qei2iC-vB6Yg/exec";
+const BASE_URL = "https://script.google.com/macros/s/AKfycbyZwR4e2i08l3bsvOa31wKMW2lxNZjTldTHEkB2HP62XCT8hEB0o3AtUTHCIjPPjHG4kA/exec";
